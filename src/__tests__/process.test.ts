7de2b4337a9ddd8cdc7c06954ee8ad9e67af2ba4
Daniele Bolla <bolladaniele@gmail.com>
2025-07-17 15:47:32 +0100

    adds another simple process-input test

--- src/__tests__/process.test.ts
+++ src/__tests__/process.test.ts
@@ -1,10 +1,16 @@
-import { describe, it, expect } from 'vitest';
-import { processInput } from '../process/input.process';
+import { describe, it, expect } from "vitest";
+import { processInput } from "../process/input.process";
 
-describe('process', () => {
-  it('processInput should correctly process a simple input string', () => {
+describe("process", () => {
+  it("processInput should correctly process a simple input string", () => {
     const input = `5 5\n1 2 N\nLMLMLMLMM`;
-    const expectedOutput = ['1 3 N'];
+    const expectedOutput = ["1 3 N"];
+    const result = processInput(input);
+    expect(result).toEqual(expectedOutput);
+  });
+  it("processInput should correctly process a input string, other example", () => {
+    const input = `3 3\n1 1 E\nMRM`;
+    const expectedOutput = ["2 0 S"];
     const result = processInput(input);
     expect(result).toEqual(expectedOutput);
   });
